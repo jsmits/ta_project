83667751724fe3c1a860fb2edb2e8517804b0b4e
jsmits <jsmits@7961b8e5-0c0d-0410-93b5-baf7301955fc>
2007-10-01 20:02:44 +0000

    git-svn-id: https://svn.huttt.com/ta_project@1703 7961b8e5-0c0d-0410-93b5-baf7301955fc

--- src/logger.py
+++ src/logger.py
@@ -3,42 +3,34 @@ import logging.handlers
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-trh = logging.handlers.RotatingFileHandler('../log/ta.log', 'a', 10 * 1024 * 1024, 10)
-trh.setLevel(logging.DEBUG)
-trh.setFormatter(formatter)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+
+fh = logging.handlers.RotatingFileHandler('../log/ta.log', 'a', 10 * 1024 * 1024, 10)
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
 
 # add handler to stomp.py logger
 logger_A = logging.getLogger("stomp.py")
 logger_A.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(formatter)
 logger_A.addHandler(ch)
-logger_A.addHandler(trh)
+logger_A.addHandler(fh)
 
 # add handler to messaging logger
 mlogger = logging.getLogger("messaging")
 mlogger.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(formatter)
 mlogger.addHandler(ch)
-mlogger.addHandler(trh)
+mlogger.addHandler(fh)
 
 # configure server Logger
 server_logger = logging.getLogger("server")
 server_logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(formatter)
 server_logger.addHandler(ch)
-server_logger.addHandler(trh)
+server_logger.addHandler(fh)
 
 # configure server Logger
 client_logger = logging.getLogger("client")
 client_logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(formatter)
 client_logger.addHandler(ch)
-client_logger.addHandler(trh)
+client_logger.addHandler(fh)
