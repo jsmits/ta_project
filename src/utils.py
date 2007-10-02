2bf2920ed1440e8a766e14a7b8439617204b3813
jsmits <jsmits@7961b8e5-0c0d-0410-93b5-baf7301955fc>
2007-10-02 20:45:19 +0000

    git-svn-id: https://svn.huttt.com/ta_project@1708 7961b8e5-0c0d-0410-93b5-baf7301955fc

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import pickle as pickle # pickle does a faster encoding than cPickle for jython !!!
 import base64
+from datetime import datetime
+import time
  
 def message_encode(obj):
     dp = pickle.dumps(obj, pickle.HIGHEST_PROTOCOL)
@@ -10,3 +12,13 @@ def message_decode(obj):
     dd = base64.decodestring(obj)
     du = pickle.loads(dd)
     return du
+
+def tick_boundaries(timestamp, period):
+    # timestamp in seconds from 1970-01-01 00:00:00 (UTC)
+    date = datetime.fromtimestamp(timestamp)
+    minute = date.minute
+    sm = minute - minute % period
+    start_date = datetime(date.year, date.month, date.day, date.hour, sm, 0)
+    start_timestamp = time.mktime(start_date.timetuple())
+    end_timestamp = start_timestamp + (period * 60)
+    return start_timestamp, end_timestamp
